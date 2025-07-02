20ab27b630f40038b878077ee7457b08f7a21ea2
tshami218 <welterial@gmail.com>
2025-07-02 21:42:38 +0700

    Create UI FOR studentWINDOW

---
+++ PT2/Views/Students/MainStudentWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PT2.Views.Students
+{
+    /// <summary>
+    /// Interaction logic for MainStudentWindow.xaml
+    /// </summary>
+    public partial class MainStudentWindow : Window
+    {
+        public MainStudentWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
